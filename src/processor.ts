f299b0f05a61276924a59a1b387569e321b77ac5
0xbe1 <0xbetrue@gmail.com>
2022-10-13 21:09:32 +0800

    add borrow metrics

--- src/processor.ts
+++ src/processor.ts
@@ -1,7 +1,19 @@
-import { oracle } from "./types/aptos/mod";
+import { oracle, vault } from "./types/aptos/mod";
 
 oracle
   .bind({ ...oracle.DEFAULT_OPTIONS, startVersion: 277151455 })
   .onEntryUpdate((call, ctx) => {
     ctx.meter.Counter("count_update_oracle").add(1);
+  });
+
+vault
+  .bind({ ...vault.DEFAULT_OPTIONS, startVersion: 278887758 })
+  .onEventBorrowEvent((event, ctx) => {
+    ctx.meter.Counter("count_borrow").add(1);
+    ctx.meter
+      .Counter("cumulative_borrow_amount")
+      .add(Number(event.data.amount) / 10 ** 8);
+    ctx.meter
+      .Counter("cumulative_borrow_fee")
+      .add(Number(event.data.fee) / 10 ** 8);
   });
