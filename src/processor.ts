e89f2278b7525e45d3963da5de8268d683e326f3
0xbe1 <0xbetrue@gmail.com>
2022-10-13 15:44:36 +0800

    souffle example working

---
+++ src/processor.ts
@@ -0,0 +1,19 @@
+import { SouffleChefCampaign } from './types/aptos/souffle'
+
+SouffleChefCampaign.bind({ ...SouffleChefCampaign.DEFAULT_OPTIONS, startVersion: 6604913 })
+  .onEntryPullTokenV2((call, ctx) => {
+    ctx.meter.Counter('call_num').add(1)
+    ctx.meter.Counter('pulled').add(parseInt(call.arguments[3]))
+  })
+  .onEventBurnEnjoyEvent((evt, ctx) => {
+    ctx.meter.Counter('burned').add(1)
+  })
+  .onTransaction((txn, ctx) => {
+    if (txn.events) {
+      for (const event of txn.events) {
+        if (event && event.type === '0x3::token::DepositEvent') {
+          ctx.meter.Counter('deposit_token_count').add(Number(event.data.amount))
+        }
+      }
+    }
+  })
