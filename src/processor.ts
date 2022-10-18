0ebc15bbf7335c85ae387829261992463531c056
0xbe1 <0xbetrue@gmail.com>
2022-10-18 12:41:42 +0800

    feat: more vault events

--- src/processor.ts
+++ src/processor.ts
@@ -6,18 +6,46 @@ oracle.bind({ startVersion: START_VERSION }).onEntryUpdate((call, ctx) => {
   ctx.meter.Counter("count_update_oracle").add(1);
 });
 
-vault.bind({ startVersion: START_VERSION }).onEventBorrowEvent((event, ctx) => {
-  ctx.meter.Counter("count_borrow").add(1);
-  ctx.meter
-    .Counter("cumulative_borrow_amount")
-    .add(event.data_typed.amount, {
+vault
+  .bind({ startVersion: START_VERSION })
+  .onEventBorrowEvent((event, ctx) => {
+    ctx.meter.Counter("count_borrow").add(1, {
+      coin: event.type_arguments[0],
+    });
+    ctx.meter.Counter("cumulative_borrow_amount").add(event.data_typed.amount, {
       coin: event.type_arguments[0],
       // borrower: ctx.transaction.sender,
     });
-  ctx.meter
-    .Counter("cumulative_borrow_fee")
-    .add(event.data_typed.fee, {
+    ctx.meter.Counter("cumulative_borrow_fee").add(event.data_typed.fee, {
       coin: event.type_arguments[0],
       // borrower: ctx.transaction.sender,
     });
-});
+  })
+  .onEventRepayEvent((event, ctx) => {
+    ctx.meter.Counter("count_repay").add(1, {
+      coin: event.type_arguments[0],
+    });
+    ctx.meter.Counter("cumulative_repay_amount").add(event.data_typed.amount, {
+      coin: event.type_arguments[0],
+    });
+  })
+  .onEventDepositEvent((event, ctx) => {
+    ctx.meter.Counter("count_deposit_collateral").add(1, {
+      coin: event.type_arguments[0],
+    });
+    ctx.meter
+      .Counter("cumulative_deposit_collateral_amount")
+      .add(event.data_typed.amount, {
+        coin: event.type_arguments[0],
+      });
+  })
+  .onEventWithdrawEvent((event, ctx) => {
+    ctx.meter.Counter("count_withdraw_collateral").add(1, {
+      coin: event.type_arguments[0],
+    });
+    ctx.meter
+      .Counter("cumulative_withdraw_collateral_amount")
+      .add(event.data_typed.amount, {
+        coin: event.type_arguments[0],
+      });
+  });
