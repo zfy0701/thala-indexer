642de74c679d7bcdda4200e00d5d970ab03b7c67
0xbe1 <0xbetrue@gmail.com>
2022-12-28 10:36:56 +0800

    fix mod indexer by bumping sentio sdk

--- src/processors/mod.ts
+++ src/processors/mod.ts
@@ -1,16 +1,16 @@
 import { stability_pool, vault } from "../types/aptos/testnet/mod";
 import { scaleDown, getCoinDecimals } from "../utils";
 
-import { Gauge } from "@sentio/sdk";
-import { Exporter } from "@sentio/sdk/lib/core/exporter";
-import { getPriceBySymbol, getPriceByType } from "@sentio/sdk/lib/utils/price";
-import { APTOS_MAINNET_ID } from "@sentio/sdk/lib/utils/chain";
+// import { Gauge } from "@sentio/sdk";
+// import { Exporter } from "@sentio/sdk/lib/core/exporter";
+// import { getPriceBySymbol, getPriceByType } from "@sentio/sdk/lib/utils/price";
+// import { APTOS_MAINNET_ID } from "@sentio/sdk/lib/utils/chain";
 
 const START_VERSION = 378452652;
 const MOD_DECIMALS = 8;
 
-const exporter = Exporter.register("VaultUpdated", "UpdateSortedVaults");
-const coinPriceGauge = Gauge.register("coin_price", { sparse: true });
+// const exporter = Exporter.register("VaultUpdated", "UpdateSortedVaults");
+// const coinPriceGauge = Gauge.register("coin_price", { sparse: true });
 
 export function processor() {
   vault
@@ -61,38 +61,38 @@ export function processor() {
         .add(scaleDown(event.data_typed.amount, getCoinDecimals(coin)), {
           coin,
         });
-    })
-    .onEventVaultUpdatedEvent(async (event, ctx) => {
-      let coinType = event.type_arguments[0];
-      let price = 0;
-      if (
-        coinType ===
-        "0x16fe2df00ea7dde4a63409201f7f4e536bde7bb7335526a35d05111e68aa322c::TestCoinsV1::BTC"
-      ) {
-        price = await getPriceBySymbol(
-          "BTC",
-          new Date(Number(ctx.transaction.timestamp) / 1000)
-        );
-      } else {
-        price = await getPriceByType(
-          // use mainnet price is fine
-          APTOS_MAINNET_ID,
-          coinType,
-          new Date(Number(ctx.transaction.timestamp) / 1000)
-        );
-      }
-
-      coinPriceGauge.record(ctx, price, { coin: coinType });
-
-      const data = {
-        version: event.version,
-        account: event.data_typed.vault_address,
-        coinType,
-        collateral: event.data_typed.collateral,
-        debt: event.data_typed.debt,
-      };
-      exporter.emit(ctx, data);
     });
+  // .onEventVaultUpdatedEvent(async (event, ctx) => {
+  //   let coinType = event.type_arguments[0];
+  //   let price = 0;
+  //   if (
+  //     coinType ===
+  //     "0x16fe2df00ea7dde4a63409201f7f4e536bde7bb7335526a35d05111e68aa322c::TestCoinsV1::BTC"
+  //   ) {
+  //     price = await getPriceBySymbol(
+  //       "BTC",
+  //       new Date(Number(ctx.transaction.timestamp) / 1000)
+  //     );
+  //   } else {
+  //     price = await getPriceByType(
+  //       // use mainnet price is fine
+  //       APTOS_MAINNET_ID,
+  //       coinType,
+  //       new Date(Number(ctx.transaction.timestamp) / 1000)
+  //     );
+  //   }
+
+  //   coinPriceGauge.record(ctx, price, { coin: coinType });
+
+  //   const data = {
+  //     version: event.version,
+  //     account: event.data_typed.vault_address,
+  //     coinType,
+  //     collateral: event.data_typed.collateral,
+  //     debt: event.data_typed.debt,
+  //   };
+  //   exporter.emit(ctx, data);
+  // });
 
   stability_pool
     .bind({ startVersion: START_VERSION })
