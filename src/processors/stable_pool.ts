53642c9632e8f6e01fca0fea4a9d786b5fbf913a
0xbe1 <0xbetrue@gmail.com>
2023-03-04 11:09:43 +0800

    Revert "feat(tha-766): integrate coingecko metrics (#90)"
    This reverts commit bdd1c728871dd631f2878f79d5835cb133e38c33.

--- src/processors/stable_pool.ts
+++ src/processors/stable_pool.ts
@@ -24,7 +24,7 @@ stable_pool
   .onEventSwapEvent(
     async (event: stable_pool.SwapEventInstance, ctx: AptosContext) => {
       const coins = getCoins(event);
-      const poolType = getPoolType(event);
+      const poolTag = getPoolTag(coins);
 
       const relativePrices = getRelativePrices(event, coins);
 
@@ -32,7 +32,7 @@ stable_pool
         ctx,
         "stable",
         coins,
-        poolType,
+        poolTag,
         relativePrices,
         event.data_decoded.idx_in,
         event.data_decoded.idx_out,
@@ -182,6 +182,20 @@ function getCoins(
 
 function isNullType(typeArg: string): boolean {
   return typeArg === NULL_TYPE;
+}
+
+// use "SP-123456coin0Name-100000coin1Name-200000coin2Name-300000coin3Name" as unique tag for each pool
+// the first 6 digits of coin address are used to reduce the length of the tag
+// TODO: poolTag was a workaround for older sentio sdk that did not support long tags
+// now tag string can be up to 512 characters. we can migrate both frontend and indexer to use full pool type (getPoolType)
+function getPoolTag(coins: string[]): string {
+  const concatCoins = coins
+    .map((coin) => {
+      const fragments = coin.split("::");
+      return fragments[0].slice(2, 8) + fragments[fragments.length - 1];
+    })
+    .join("-");
+  return `SP-${concatCoins}`;
 }
 
 // get complete pool type name. notice: there's a space after ", ". example:
